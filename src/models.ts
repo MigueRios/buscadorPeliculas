820790d989ea872c6cb9c79435808abbc93eeaa7
MigueRios <161901091+MigueRios@users.noreply.github.com>
2025-02-13 15:29:37 -0500

    Carga de archivos

---
+++ src/models.ts
@@ -0,0 +1,23 @@
+import * as jsonfile from "jsonfile";
+// El siguiente import no se usa pero es necesario
+import "./pelis.json";
+// de esta forma Typescript se entera que tiene que incluir
+// el .json y pasarlo a la carpeta /dist
+// si no, solo usandolo desde la libreria jsonfile, no se dá cuenta
+
+// no modificar estas propiedades, agregar todas las que quieras
+class Peli {
+  id: number;
+  title: string;
+  tags: string[];
+}
+
+class PelisCollection {
+  getAll(): Promise<Peli[]> {
+    return jsonfile.readFile("...laRutaDelArchivo").then(() => {
+      // la respuesta de la promesa
+      return [];
+    });
+  }
+}
+export { PelisCollection, Peli };
